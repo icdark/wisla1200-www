36c92947ec90beaeb68123ebf9cb4401d25999f0
Kamil Bujniewicz <kamil.bujniewicz@gmail.com>
2026-06-26 13:58:32 +0200

    reorder tabs

--- app.js
+++ app.js
@@ -128,6 +128,7 @@ function init() {
   const defaultDate = defaultSelectedDate();
   dateSelect.value = defaultDate;
   loadForecastsForDay(defaultDate);
+  scheduleRideCalculation();
 }
 
 function initTabs() {
